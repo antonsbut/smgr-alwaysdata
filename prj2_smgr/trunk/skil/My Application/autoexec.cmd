9c14bacae9b76c557f56d7fa0da4ab3693d23340
andraylsol <andraylsol@gmail.com>
2013-08-02 09:59:09 +0000

    Исправлены форматы времени в UTC.

--- prj2_smgr/trunk/skil/My Application/autoexec.cmd
+++ prj2_smgr/trunk/skil/My Application/autoexec.cmd
@@ -3,6 +3,6 @@ rem cd  c:\skill\"my application"
 rem start  rgpn.exe
 schtasks /query /tn watchdog
 if not errorlevel 1 goto end
-schtasks /create /sc minute /mo 3 /tn watchdog /tr "'c:\skill\my application\qw.vbs'"
+schtasks /create /sc minute /mo 3 /tn watchdog /tr "'c:\work\lv2010\skil\skil\my application\qw.vbs'"
 :end
 rem qw.vbs
